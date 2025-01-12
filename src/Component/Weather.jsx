c9c0e51524b7e1f059eb0f91d45e503ce76b3738
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2025-01-12 23:51:15 +0500

    features added

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -11,26 +11,39 @@ function Weather() {
   let ref = useRef(null);
   let [celcius, setCelcius] = useState("");
   let [fahranhiet, setFahrenhiet] = useState("");
+  let [error, setError] = useState("");
+
+
+  // function errorRendering() {
+  //   <div>
+  //     <p>{error}</p>
+  //   </div>;
+  // }
+
 
   // audio function
   let audioRef = useRef(null);
-  let [isPlay, setIsPlay] = useState(true);
+  let [isPlay, setIsPlay] = useState("");
   useEffect(() => {
     if (audioRef.current) {
       audioRef.current.volume = 0.5;
     }
+    setIsPlay(isPlay);
   }, []);
-  const playPause = () => {
+  const pause = () => {
     if (audioRef.current) {
       if (isPlay) {
         audioRef.current.pause();
-      } else {
-        audioRef.current.play();
       }
-      setIsPlay(!isPlay)
     }
+    setIsPlay(!isPlay);
   };
-
+  const play = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+    setIsPlay(isPlay);
+  };
   function text() {
     useEffect(() => {
       // Element ko reference ke zariye access karein
@@ -62,12 +75,17 @@ function Weather() {
       }
       text();
     } catch (e) {
-      console.log(e);
+      let error = e.message;
+      setError(error);
+      errorRendering();
+      console.log(error);
     }
   };
+ 
+
   function Fahranhiet(celsius) {
     const fahrenheitValue = (celsius * 9) / 5 + 32;
-    setFahrenheit(fahrenheitValue.toFixed(2));
+    setFahrenhiet(fahrenheitValue.toFixed(2));
   }
   return (
     <>
@@ -75,129 +93,137 @@ function Weather() {
         className="vh-100"
         style={{
           background: "url(./src/assets/SFFd.gif)",
-          // "url(https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/draw1.webp)",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       >
-        <i className="fa-solid fa-volume-low" style={{color: "#fafcff"}}></i>
         <audio ref={audioRef} autoPlay loop>
           <source
             src="./src/assets/mixkit-bad-weather-heavy-rain-and-thunder-1261.wav"
             type="audio/wav"
           />
         </audio>
-         <button onClick={playPause}>
-          {isPlay ? 'Pause' : 'Play'}
-        </button>
-        <div className="container py-5 h-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-md-8 col-lg-6 col-xl-4">
-              <h2 className="mb-4 pb-2 fw-bold tracking-in-expand text-success">
-                Check the weather forecast
-              </h2>
-
-              <div className="input-group rounded mb-3">
-                <input
-                  type="search"
-                  className="form-control rounded"
-                  placeholder="City"
-                  aria-label="Search"
-                  aria-describedby="search-addon"
-                  ref={userCityRef}
-                />
-                <a href="#!" type="button">
-                  <span
-                    className="input-group-text border-0 fw-bold"
-                    id="search-addon"
-                    onClick={getCityName}
-                  >
-                    search
-                  </span>
-                </a>
-              </div>
-
-              {data && (
-                <div
-                  className="card shadow-0  fade-in-left "
-                  style={{ backgroundColor: "gray" }}
-                >
-                  <div className="my-4 px-4">
-                    <div className="form-check form-check-inline">
-                      <input
-                        className="form-check-input"
-                        type="radio"
-                        name="inlineRadioOptions"
-                        id="inlineRadio1"
-                        value="option1"
-                        checked
-                      />
-                      <label
-                        className="form-check-label text-dark"
-                        htmlFor="inlineRadio1"
-                      >
-                        Celsius
-                      </label>
-                    </div>
-
-                    <div className="form-check form-check-inline">
-                      <input
-                        className="form-check-input"
-                        type="radio"
-                        name="inlineRadioOptions"
-                        id="inlineRadio2"
-                        value="option2"
-                      />
-                      <label
-                        className="form-check-label text-dark"
-                        htmlFor="inlineRadio2"
-                        onClick={Fahranhiet}
-                      >
-                        Farenheit
-                      </label>
-                    </div>
-                  </div>
-                  <div className="card-body p-4">
-                    <h4 className="mb-1 sfw-normal text-focus-in">
-                      {data?.name}
-                    </h4>
-                    <p className="mb-2 ">
-                      Current temperature:{" "}
-                      <strong ref={ref}>{data?.main?.temp}°C</strong>
-                    </p>
-                    <p>
-                      Feels like:{" "}
-                      <strong ref={ref}>{data?.main?.feels_like}°C</strong>
-                    </p>
-                    <p>
-                      Max: <strong ref={ref}>{data?.main?.temp_max}°C</strong>,
-                      Min: <strong ref={ref}>{data?.main?.temp_min}°C</strong>
-                    </p>
-                    <p>
-                      Humidity:{" "}
-                      <strong ref={ref}>{data?.main?.humidity}</strong>
-                    </p>
-                    <p>
-                      sea Level: <strong ref={ref}>{altitude}</strong>
-                    </p>
-                    <p>
-                      Wind speed: <strong ref={ref}>{data?.wind?.speed}</strong>
-                    </p>
-                    <img src={`http://openweathermap.org/img/w/${icon}.png`} />
-
-                    <div className="d-flex flex-row align-items-center">
-                      <i
-                        className="fas fa-cloud fa-3x"
-                        style={{ color: "#eee" }}
-                      ></i>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
+        <div className="container  h-100">
+          <div className="d-flex gap-3 pt-4">
+            <i
+              className="fa-solid fa-volume-low"
+              onClick={pause}
+              style={{ color: "#fafcff" }}
+            ></i>
+            <i
+              className="fa-solid fa-volume-high"
+              onClick={play}
+              style={{ color: "#fafcff" }}
+            ></i>
           </div>
+          <div className="text-center">
+            <h1 className="mb-4 pb-2 fw-bold tracking-in-expand text-white">
+              Check the weather forecast
+            </h1>
+          </div>
+          <div className="input-group rounded mb-3 gap-5">
+            <input
+              type="search"
+              className="form-control rounded "
+              style={{ background: "#F7F7F7" }}
+              placeholder="City"
+              aria-label="Search"
+              aria-describedby="search-addon"
+              ref={userCityRef}
+            />
+            <a href="#!" type="button">
+              <span
+                className="input-group-text border-0 fw-bold  "
+                id="search-addon"
+                onClick={getCityName}
+              >
+                search
+              </span>
+            </a>
+          </div>
+
+          {error&&(
+            <div className="text-white text-center" style={{fontSize:"40px"}}>{error}</div>
+          )}
+
+          {/* <div className="row d-flex justify-content-center align-items-center "> */}
+          {data && (
+            <div
+              className="card shadow-0 mt-5  fade-in-left "
+              style={{ backgroundColor: "url(./src/assets/QlQV.gif" }}
+            >
+              <div className="my-4 px-4">
+                <div className="form-check form-check-inline">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="inlineRadioOptions"
+                    id="inlineRadio1"
+                    value="option1"
+                    checked
+                  />
+                  <label
+                    className="form-check-label text-dark"
+                    htmlFor="inlineRadio1"
+                  >
+                    Celsius
+                  </label>
+                </div>
+
+                <div className="form-check form-check-inline">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="inlineRadioOptions"
+                    id="inlineRadio2"
+                    value="option2"
+                  />
+                  <label
+                    className="form-check-label text-dark"
+                    htmlFor="inlineRadio2"
+                    onClick={Fahranhiet}
+                  >
+                    Farenheit
+                  </label>
+                </div>
+              </div>
+              <div className="card-body p-4">
+                <h4 className="mb-1 sfw-normal text-focus-in">{data?.name}</h4>
+                <p className="mb-2 ">
+                  Current temperature:{" "}
+                  <strong ref={ref}>{data?.main?.temp}°C</strong>
+                </p>
+                <p>
+                  Feels like:{" "}
+                  <strong ref={ref}>{data?.main?.feels_like}°C</strong>
+                </p>
+                <p>
+                  Max: <strong ref={ref}>{data?.main?.temp_max}°C</strong>, Min:{" "}
+                  <strong ref={ref}>{data?.main?.temp_min}°C</strong>
+                </p>
+                <p>
+                  Humidity: <strong ref={ref}>{data?.main?.humidity}</strong>
+                </p>
+                <p>
+                  sea Level: <strong ref={ref}>{altitude}</strong>
+                </p>
+                <p>
+                  Wind speed: <strong ref={ref}>{data?.wind?.speed}</strong>
+                </p>
+                <img src={`http://openweathermap.org/img/w/${icon}.png`} />
+
+                <div className="d-flex flex-row align-items-center">
+                  <i
+                    className="fas fa-cloud fa-3x"
+                    style={{ color: "#eee" }}
+                  ></i>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
+        {/* </div> */}
       </section>
 
       {/* <div>
